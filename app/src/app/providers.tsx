b77ec740f749bfb51f698c7d9046bcc25664c0aa
JD <jann.driessen@gmx.de>
2024-03-15 21:56:55 +0000

    add providers and connectkit config

---
+++ app/src/app/providers.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { base } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    chains: [base],
+    transports: {
+      [base.id]: http(
+        `https://base-mainnet.g.alchemy.com/v2/${process.env
+          .NEXT_PUBLIC_ALCHEMY_ID!}`
+      ),
+    },
+
+    // Required API Keys
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+
+    appName: "warp",
+    // Optional
+    appDescription: "warp warp",
+    appUrl: "https://warp.xyz", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
+);
+
+const queryClient = new QueryClient();
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export const Providers = ({ children }: ProvidersProps) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
