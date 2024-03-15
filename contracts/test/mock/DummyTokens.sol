4214b40f4fea2c7662c1849d974e2c5cac5e93b3
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-03-15 22:33:42 +0000

    DummyTokens added

---
+++ contracts/test/mock/DummyTokens.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MockCCIPBnMToken is ERC20 {
+    constructor() ERC20("Mock CCIP-BnM", "mCCIP-BnM") {
+        _mint(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 1000e18); // Anvil 0
+    }
+}
+
+contract MockTestToken is ERC20 {
+    constructor() ERC20("TestToken", "TEST") {
+        _mint(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 1000e18); // Anvil 0
+    }
+}
+
+contract MockLinkToken is ERC20 {
+    constructor() ERC20("Mock Link", "mLINK") {
+        _mint(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266, 1000e18); // Anvil 0
+    }
+}
+
+contract MockTokenDeployer {
+    MockCCIPBnMToken public mockCCIPBnM;
+    MockTestToken public mockTest;
+    MockLinkToken public mockLink;
+
+    constructor() {
+        mockCCIPBnM = new MockCCIPBnMToken();
+        mockTest = new MockTestToken();
+        mockLink = new MockLinkToken();
+    }
+}
