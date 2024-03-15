0d0bdf995cb0a02ac1a9d558f97440b8dfa4af8b
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-03-15 22:58:56 +0000

    ILogAutomation and log struct added

--- contracts/src/SourceVault.sol
+++ contracts/src/SourceVault.sol
@@ -8,6 +8,26 @@ import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
 import {LibFormatter} from "./utils/LibFormatter.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
+struct Log {
+    uint256 index; // Index of the log in the block
+    uint256 timestamp; // Timestamp of the block containing the log
+    bytes32 txHash; // Hash of the transaction containing the log
+    uint256 blockNumber; // Number of the block containing the log
+    bytes32 blockHash; // Hash of the block containing the log
+    address source; // Address of the contract that emitted the log
+    bytes32[] topics; // Indexed topics of the log
+    bytes data; // Data of the log
+}
+
+interface ILogAutomation {
+    function checkLog(
+        Log calldata log,
+        bytes memory checkData
+    ) external returns (bool upkeepNeeded, bytes memory performData);
+
+    function performUpkeep(bytes calldata performData) external;
+}
+
 contract SourceVault is
     ProgrammableTokenTransfers,
     ERC4626 // Make this "is ILogAutomation"
@@ -148,4 +168,6 @@ contract SourceVault is
         );
         currentDeposits = 0;
     }
+
+    // AUTOMATION FUNCTIONS
 }
