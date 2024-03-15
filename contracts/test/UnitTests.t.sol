82ec84f51e811f2d925bc36b8ac5d902d632f457
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-03-15 23:12:19 +0000

    testCorrectDestChainId() added, tests working

--- contracts/test/UnitTests.t.sol
+++ contracts/test/UnitTests.t.sol
@@ -14,7 +14,6 @@ import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-sol
 import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
 
 contract UnitTests is StdCheats, Test {
-    
     ////////// CONTRACTS //////////
     ProgrammableTokenTransfers public receiver; // replace later
     SenderReceiver public senderReceiver;
@@ -36,4 +35,80 @@ contract UnitTests is StdCheats, Test {
     address public constant DEV_ACCOUNT_2 =
         0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
 
+    ////////// SETUP //////////
+    function setUp() external {
+        router = new MockCCIPRouter();
+        linkToken = new MockLinkToken();
+        ccipBnM = new MockCCIPBnMToken();
+
+        receiver = new ProgrammableTokenTransfers(
+            address(router),
+            address(linkToken)
+        );
+
+        senderReceiver = new SenderReceiver(
+            address(router),
+            address(linkToken)
+        );
+
+        sourceVault = new SourceVault(
+            ERC20(address(ccipBnM)),
+            "SourceVault",
+            "SV",
+            address(router),
+            address(linkToken)
+        );
+
+        destinationVault = new DestinationVault(
+            ERC20(address(ccipBnM)),
+            "DestinationVault",
+            "DV"
+        );
+
+        sourceVault.allowlistDestinationChain(12532609583862916517, true);
+
+        receiver.allowlistSourceChain(16015286601757825753, true);
+        receiver.allowlistSender(address(sourceVault), true);
+
+        senderReceiver.allowlistSourceChain(16015286601757825753, true);
+        senderReceiver.allowlistSender(address(sourceVault), true);
+        senderReceiver.addDestinationVault(address(destinationVault));
+        senderReceiver.addVaultToken(address(ccipBnM));
+    }
+
+    ////////// HELPER FUNCTIONS //////////
+    // transfer mocklink tokens to SourceVault from dev account 0
+
+    function transferLinkTokensToSourceVault() public {
+        vm.startPrank(DEV_ACCOUNT_0);
+
+        linkToken.transfer(address(sourceVault), TOKEN_MINT_BALANCE);
+
+        vm.stopPrank();
+    }
+
+    // function deposit tokens to SourceVault from dev account 0
+    function depositTokensToSourceVault() public {
+        vm.startPrank(DEV_ACCOUNT_0);
+
+        ccipBnM.approve(address(sourceVault), TOKEN_TRANSFER_AMOUNT);
+        sourceVault._deposit(TOKEN_TRANSFER_AMOUNT);
+
+        vm.stopPrank();
+    }
+
+    function fundReceiver() public {
+        address receiverAddress = address(receiver);
+        vm.deal(receiverAddress, TOKEN_TRANSFER_AMOUNT);
+    }
+
+    ////////// SETUP TESTS //////////
+
+    function testCorrectDestinationChainId() public {
+        //check that the destination chain id is correct on TokenTransferor
+        assertTrue(
+            sourceVault.allowlistedDestinationChains(12532609583862916517),
+            "Destination chain id is not correctly allowlisted"
+        );
+    }
 }
