e51409cf899c183a0e81ad7e13c25042081857eb
JD <jann.driessen@gmx.de>
2024-03-16 11:21:23 +0000

    add usdc to input

--- app/src/app/components/bridge/components/input.tsx
+++ app/src/app/components/bridge/components/input.tsx
@@ -1,3 +1,20 @@
+import Image from "next/image"
+
 export function Input() {
-  return <div className="flex flex-col bg-element rounded-xl h-10 w-full"></div>
+  return (
+    <div className="flex flex-col bg-element rounded-xl w-full p-4">
+      <div className="flex flew-row justify-between items-start">
+        <div></div>
+        <div className="flex flex-row gap-2 items-center text-lg font-medium text-white bg-element2 px-4 py-2 rounded-full">
+          <Image
+            alt="arbitrum logo"
+            src="/assets/usdc-logo.svg"
+            width={24}
+            height={24}
+          />
+          <div>USDC</div>
+        </div>
+      </div>
+    </div>
+  )
 }
