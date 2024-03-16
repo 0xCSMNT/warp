2735c4de08cda69146dea4b3cc9b8fabbe2e91b0
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-03-16 16:52:10 +0000

    deploy scripts to mainnet finalized

--- contracts/script/DeploySrToArbitrum.s.sol
+++ contracts/script/DeploySrToArbitrum.s.sol
@@ -15,6 +15,7 @@ contract DeployContractsToArbSepolia is Script {
     address LINK = 0xf97f4df75117a78c1A5a0DBb814Af92458539FB4;
     address USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
     uint64 BASE_CHAIN_ID = 15971525489660198786;
+    uint64 ARBITRUM_CHAIN_ID = 4949039107694359620;
 
     // Change this to the address of the SourceVault
     address SOURCE_VAULT = 0x00Dea5cd7D214ebf17197d38C1C986E441be0946; // WRONG ADDRESS!
@@ -27,8 +28,12 @@ contract DeployContractsToArbSepolia is Script {
 
         senderReceiver.allowlistSourceChain(BASE_CHAIN_ID, true);
         senderReceiver.allowlistSender(SOURCE_VAULT, true);
+        senderReceiver.allowlistDestinationChain(ARBITRUM_CHAIN_ID, true);
         senderReceiver.addDestinationVault(address(DESTINATION_VAULT)); // SOMMELIER
         senderReceiver.addVaultToken(USDC);
+        senderReceiver.updateVaultTokenDecimals(6);
+        senderReceiver.addSourceChainId(BASE_CHAIN_ID);
+        senderReceiver.addSourceVault(SOURCE_VAULT);
 
         vm.stopBroadcast();
     }
