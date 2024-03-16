787b7627e3578158ffee3c54b07a1ce7d0344a80
JD <jann.driessen@gmx.de>
2024-03-16 19:31:01 +0000

    add submit animation

---
+++ app/src/app/components/bridge/components/submitting.tsx
@@ -0,0 +1,17 @@
+export function Submitting() {
+  return (
+    <div className="bg-black flex flex-row rounded-xl h-full justify-center items-end p-10 gap-6">
+      <iframe
+        src="https://giphy.com/embed/MaThe6p8WAKbf9NDDM"
+        width="100px"
+        height="100px"
+        className="giphy-embed"
+        allowFullScreen
+      ></iframe>
+      <div>
+        <div className="text-white font-extralight text-lg">{`Base -> Arbitrum`}</div>
+        <div className="text-white font-medium text-lg">{`100 USDC -> 100 Real Yield USDC`}</div>
+      </div>
+    </div>
+  )
+}
