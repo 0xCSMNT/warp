a9efda2264b2842decca7150af4207dee49e91c4
JD <jann.driessen@gmx.de>
2024-03-16 08:34:17 +0000

    add footer

---
+++ app/src/app/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import { ConnectKitButton } from "connectkit"
+
+export function Footer() {
+  return (
+    <footer className="flex items-end justify-between w-full p-10">
+      <div className="italic font-bold text-2xl">warp ⊹</div>
+      <ConnectKitButton />
+    </footer>
+  )
+}
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,19 @@
-"use client";
+"use client"
 
-import { ConnectKitButton } from "connectkit";
+import { Footer } from "@/app/components/footer"
+import { ConnectKitButton } from "connectkit"
+import { useAccount } from "wagmi"
 
 export default function Connect() {
+  const { isConnected } = useAccount()
   return (
     <main className="flex justify-center items-center w-full h-screen">
-      <ConnectKitButton />
+      <div className="flex flex-col justify-between items-center h-full w-full">
+        <div></div>
+        <ConnectKitButton />
+        {isConnected && <Footer />}
+        {!isConnected && <div></div>}
+      </div>
     </main>
-  );
+  )
 }
