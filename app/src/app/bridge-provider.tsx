0571b90147dbd3df7a6365b416d09b84dd0200e9
JD <jann.driessen@gmx.de>
2024-03-16 15:16:15 +0000

    add approve

--- app/src/app/bridge-provider.tsx
+++ app/src/app/bridge-provider.tsx
@@ -1,5 +1,14 @@
-import { createContext, useContext, useEffect, useState } from "react"
-import { parseUnits } from "viem"
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react"
+import { parseAbi, parseUnits } from "viem"
+import { usePublicClient, useWalletClient } from "wagmi"
+
+import { BaseSourceVaultContract, USDC } from "@/app/constants"
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
@@ -13,7 +22,9 @@ export interface BridgeContext {
   inputAmountUsd: string
   isLoading: boolean
   quote: FormattedQuote | null
+  onApprove: () => void
   onChangeInput: (val: string) => void
+  onSubmit: () => void
 }
 
 export const BridgeProviderContext = createContext<BridgeContext>({
@@ -21,19 +32,55 @@ export const BridgeProviderContext = createContext<BridgeContext>({
   inputAmountUsd: "$0",
   isLoading: false,
   quote: null,
+  onApprove: () => {},
   onChangeInput: () => {},
+  onSubmit: () => {},
 })
 
 export const useBridge = () => useContext(BridgeProviderContext)
 
 export function BridgeProvider(props: { children: any }) {
+  const publicClient = usePublicClient()
+  const { data: walletClient } = useWalletClient()
+
   const [inputAmount, setInputAmount] = useState("0")
   const [inputAmountUsd, setInputAmountUsd] = useState("")
+  const [isApproving, setIsApproving] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const [quote, setQuote] = useState<FormattedQuote | null>(null)
 
+  const onApprove = useCallback(() => {
+    if (!publicClient || !walletClient) return
+    const amnt = Number(inputAmount)
+    if (amnt <= 0) return
+    const approve = async () => {
+      setIsApproving(true)
+      const abi = parseAbi([
+        "function approve(address spender, uint256 amount) external returns (bool)",
+      ])
+      const amount = parseUnits(inputAmount, 6)
+      const spender = BaseSourceVaultContract
+      const hash = await walletClient.writeContract({
+        address: USDC,
+        abi,
+        functionName: "approve",
+        args: [spender, amount],
+      })
+      await publicClient.waitForTransactionReceipt({
+        confirmations: 1,
+        hash,
+      })
+      setIsApproving(false)
+    }
+    approve()
+  }, [inputAmount, publicClient, walletClient])
+
   const onChangeInput = (val: string) => {
     setInputAmount(val)
+  }
+
+  const onSubmit = () => {
+    console.log("submit")
   }
 
   useEffect(() => {
@@ -68,7 +115,9 @@ export function BridgeProvider(props: { children: any }) {
         inputAmountUsd,
         isLoading,
         quote,
+        onApprove,
         onChangeInput,
+        onSubmit,
       }}
     >
       {props.children}
--- app/src/app/components/bridge/index.tsx
+++ app/src/app/components/bridge/index.tsx
@@ -4,8 +4,15 @@ import { Quote } from "./components/quote"
 import { useBridge } from "@/app/bridge-provider"
 
 export function Bridge() {
-  const { inputAmount, inputAmountUsd, isLoading, quote, onChangeInput } =
-    useBridge()
+  const {
+    inputAmount,
+    inputAmountUsd,
+    isLoading,
+    quote,
+    onApprove,
+    onChangeInput,
+    onSubmit,
+  } = useBridge()
   return (
     <div className="flex flex-col w-[500px] gap-1">
       <div className="flex flex-row gap-4 mb-2 ml-4">
@@ -23,10 +30,16 @@ export function Bridge() {
       {!isLoading && quote && (
         <Quote amount={quote.outputAmount} amountUsd={quote.outputAmountUsd} />
       )}
-      <div className="flex h-13 bg-element w-full items-center p-4 justify-center rounded-full text-xl font-base cursor-pointer text-white">
+      <div
+        className="flex h-13 bg-element w-full items-center p-4 justify-center rounded-full text-xl font-base cursor-pointer text-white"
+        onClick={onApprove}
+      >
         Approve
       </div>
-      <div className="flex h-13 bg-highlight w-full items-center p-4 justify-center rounded-full text-xl font-base cursor-pointer">
+      <div
+        className="flex h-13 bg-highlight w-full items-center p-4 justify-center rounded-full text-xl font-base cursor-pointer"
+        onClick={onSubmit}
+      >
         Submit
       </div>
     </div>
