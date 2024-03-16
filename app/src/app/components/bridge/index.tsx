84a23d0068502797bd983cc070f7b63ad4ab696f
JD <jann.driessen@gmx.de>
2024-03-16 14:13:13 +0000

    update margins

--- app/src/app/components/bridge/index.tsx
+++ app/src/app/components/bridge/index.tsx
@@ -6,7 +6,7 @@ export function Bridge() {
   const [input, setInput] = useState("0")
   return (
     <div className="flex flex-col w-[500px] gap-1">
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-4 mb-2 ml-4">
         <div className="text-3xl text-highlight cursor-pointer">Deposit</div>
         <div className="text-3xl text-white cursor-pointer">Withdraw</div>
       </div>
