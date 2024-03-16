52fa94f34d2fe2dc3c11d05522949a29a51c3393
JD <jann.driessen@gmx.de>
2024-03-16 10:13:27 +0000

    add network logos

--- app/src/app/components/vault/index.tsx
+++ app/src/app/components/vault/index.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx"
+import Image from "next/image"
 
 interface VaultProps {
+  networkArbitrum: boolean
   name: string
   provider: string
   apy: string
@@ -21,20 +23,31 @@ export function Vault(props: VaultProps) {
       )}
     >
       <div className={clsx("absolute inset-0 rounded-lg blur", styles.bg)} />
-      <div className="relative px-6 py-4 shadow-inner rounded-3xl flex flex-col justify-between h-full">
+      <div className="relative px-6 py-4 shadow-inner rounded-3xl flex flex-col justify-between h-full w-full">
         <div>
           <div className="font-light text-sm">{props.provider}</div>
           <div className="font-bold text-3xl">{props.name}</div>
         </div>
-        <div className="flex flex-row justify-between items-start">
+        <div className="flex flex-row justify-between items-end w-full">
           <div className="flex flex-col items-start text-lg">
             <div className="font-light">APY</div>
             <div className="font-semibold">{props.apy}</div>
           </div>
-          {/* <div className="flex flex-col items-start text-lg">
-            <div className="font-light">TVL</div>
-            <div className="font-semibold">28.46%</div>
-          </div> */}
+          {props.networkArbitrum ? (
+            <Image
+              alt="arbitrum logo"
+              src="/assets/arbitrum-logo.svg"
+              width={26}
+              height={26}
+            />
+          ) : (
+            <Image
+              alt="ethereum logo"
+              src="/assets/ethereum-logo.png"
+              width={16}
+              height={16}
+            />
+          )}
         </div>
       </div>
     </div>
--- app/src/app/components/vaults/index.tsx
+++ app/src/app/components/vaults/index.tsx
@@ -4,18 +4,21 @@ export function Vaults() {
   return (
     <div className="flex flex-row gap-10">
       <Vault
+        networkArbitrum={true}
         name="Real Yield USD"
         provider="Sommelier Finance"
         apy="38.96%"
         styles={{ bg: "bg-[#4361EE]", text: "text-white" }}
       />
       <Vault
+        networkArbitrum={true}
         name="gUSDC"
         provider="Gains"
         apy="25.6%"
         styles={{ bg: "bg-[#3A0CA3]", text: "text-white" }}
       />
       <Vault
+        networkArbitrum={false}
         name="maUSDC"
         provider="Morpho"
         apy="19.27%"
