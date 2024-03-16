6c8c6fdf5fc7ddcf0b832ea0dd191296971f2691
JD <jann.driessen@gmx.de>
2024-03-16 09:39:13 +0000

    add props

--- app/src/app/components/vault/index.tsx
+++ app/src/app/components/vault/index.tsx
@@ -4,24 +4,24 @@ interface VaultProps {
   apy: string
 }
 
-export function Vault() {
+export function Vault(props: VaultProps) {
   return (
-    <div className="bg-[#4361EE] flex items-start rounded-3xl shadow-lg h-60 w-60 relative cursor-pointer">
+    <div className="bg-[#4361EE] flex items-start rounded-3xl shadow-lg h-60 w-60 relative cursor-pointer text-background">
       <div className="absolute inset-0 bg-[#4361EE] rounded-lg blur" />
       <div className="relative px-6 py-4 shadow-inner rounded-3xl flex flex-col justify-between h-full">
         <div>
-          <div className="font-light text-xs">Sommelier Finance</div>
-          <div className="font-bold text-3xl">Real Yield USD</div>
+          <div className="font-light text-sm">{props.provider}</div>
+          <div className="font-bold text-3xl">{props.name}</div>
         </div>
         <div className="flex flex-row justify-between items-start">
           <div className="flex flex-col items-start text-lg">
             <div className="font-light">APY</div>
-            <div className="font-semibold">28.46%</div>
+            <div className="font-semibold">{props.apy}</div>
           </div>
-          <div className="flex flex-col items-start text-lg">
+          {/* <div className="flex flex-col items-start text-lg">
             <div className="font-light">TVL</div>
             <div className="font-semibold">28.46%</div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
--- app/src/app/components/vaults/index.tsx
+++ app/src/app/components/vaults/index.tsx
@@ -3,9 +3,9 @@ import { Vault } from "@/app/components/vault"
 export function Vaults() {
   return (
     <div className="flex flex-row gap-10">
-      <Vault />
-      <Vault />
-      <Vault />
+      <Vault name="Real Yield USD" provider="Sommelier Finance" apy="28.46%" />
+      <Vault name="gUSDC" provider="Gains" apy="25.6%" />
+      <Vault name="maUSDC" provider="Morpho" apy="19.27%" />
     </div>
   )
 }
