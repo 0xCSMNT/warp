dee05e4324128824d7db18e7d49c2d6c661ef3b9
JD <jann.driessen@gmx.de>
2024-03-16 11:54:54 +0000

    add quote comp

--- app/src/app/components/bridge/index.tsx
+++ app/src/app/components/bridge/index.tsx
@@ -1,8 +1,9 @@
 import { Input } from "./components/input"
+import { Quote } from "./components/quote"
 
 export function Bridge() {
   return (
-    <div className="flex flex-col w-[600px] gap-2">
+    <div className="flex flex-col w-[600px] gap-1">
       <div className="flex flex-row gap-4">
         <div className="text-3xl text-highlight cursor-pointer">Deposit</div>
         <div className="text-3xl text-white cursor-pointer">Withdraw</div>
@@ -10,6 +11,7 @@ export function Bridge() {
       <div>
         <Input />
       </div>
+      <Quote amount={"10"} usd={"$0"} />
       <div className="flex h-13 bg-element w-full items-center p-4 justify-center rounded-full text-xl font-base cursor-pointer text-white">
         Approve
       </div>
