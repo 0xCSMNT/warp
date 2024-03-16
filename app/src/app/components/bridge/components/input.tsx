87717c0910fd6b9612948cb5a0f20f6d8677dbc7
JD <jann.driessen@gmx.de>
2024-03-16 11:26:47 +0000

    add usd line

--- app/src/app/components/bridge/components/input.tsx
+++ app/src/app/components/bridge/components/input.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export function Input() {
   return (
-    <div className="flex flex-col bg-element rounded-xl w-full p-4">
+    <div className="flex flex-col bg-element rounded-xl w-full p-4 gap-6">
       <div className="flex flew-row justify-between items-start">
         <div></div>
         <div className="flex flex-row gap-2 items-center text-lg font-medium text-white bg-element2 px-4 py-2 rounded-full">
@@ -14,6 +14,9 @@ export function Input() {
           />
           <div>USDC</div>
         </div>
+      </div>
+      <div>
+        <div className="text-base text-[#9B9B9B]">$0</div>
       </div>
     </div>
   )
